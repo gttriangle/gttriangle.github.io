7ed9cad9cd99a069a9610a6d555dff332b99137a
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-20 09:30:21 -0500

    cant use scope in a svc apparently

--- sources/services/authSvc.js
+++ sources/services/authSvc.js
@@ -1,22 +1,25 @@
 (function () {
     'use strict';
-    angular.module('gt-tri').factory('authSvc', ['$q', 'firebase', '$firebaseObject', '$firebaseArray', '$scope', authSvc]);
-    function authSvc($q, firebase, $firebaseObject, $firebaseArray, $scope) {
+    angular.module('gt-tri').factory('authSvc', ['$q', 'firebase', '$firebaseObject', '$firebaseArray', authSvc]);
+    function authSvc($q, firebase, $firebaseObject, $firebaseArray) {
         var loggedInUser = {
             email: null,
             name: null,
             permission: 'unregistered'
         }
         var loggedIn = false;
-        var dict = $firebaseObject(firebase.database().ref().child('Users'));
-        dict.$bindTo($scope, "allUsers")
+        var allUsers
+        var promise = $firebaseObject(firebase.database().ref().child('Users')).$loaded();
+        promise.then(function (result) {
+
+        })
 
         var loginUser = function(email, password) {
             var defer = $q.defer();
             firebase.auth().signInWithEmailAndPassword(email, password).then(function (result) {
-                loggedInUser.email = $scope.allUsers[result.user.uid].email;
-                loggedInUser.name = $scope.allUsers[result.user.uid].name;
-                loggedInUser.permission = $scope.allUsers[result.user.uid].permission;
+                loggedInUser.email = allUsers[result.user.uid].email;
+                loggedInUser.name = allUsers[result.user.uid].name;
+                loggedInUser.permission = allUsers[result.user.uid].permission;
                 loggedIn = true;
                 defer.resolve();
             }, function (error) {
@@ -39,13 +42,15 @@
         var createUser = function(name, email, password) {
             var defer = $q.defer();
             firebase.auth().createUserWithEmailAndPassword(email, password).then(function (result) {
-                $scope.allUsers[result.user.uid] = {
+                allUsers[result.user.uid] = {
                     email: result.user.email,
                     name: name,
                     permission: 'unregistered'
                 }
-                loginUser(email, password).then(function () {
-                    defer.resolve('Success');
+                allUsers.$save().then(function () {
+                    loginUser(email, password).then(function () {
+                        defer.resolve('Success');
+                    });
                 });
             }, function (error) {
                 defer.reject(error);
