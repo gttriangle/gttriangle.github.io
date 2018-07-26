d781fb0d23e8ccb06616b71123597603f34144f6
Shourya Khare <shouryakhare@ShouryaMac.local>
2018-07-26 14:21:23 -0400

    Family Tree added. Need to center it

--- sources/controllers/FamilyTreesCtrl.js
+++ sources/controllers/FamilyTreesCtrl.js
@@ -1,7 +1,15 @@
 (function () {
     'use strict';
-    angular.module('gt-tri').controller('FamilyTreesCtrl', ['$scope', '$state', 'authSvc', FamilyTreesCtrl]);
-    function FamilyTreesCtrl($scope, $state, authSvc) {
+    angular.module('gt-tri').controller('FamilyTreesCtrl', ['$scope', '$state', 'firebase', '$firebaseObject', 'authSvc', FamilyTreesCtrl]);
+    function FamilyTreesCtrl($scope, $state, firebase, $firebaseObject, authSvc) {
 
+        $scope.loadAndTrigger = function() {
+            var promise = $firebaseObject(firebase.database().ref().child('FamilyTree')).$loaded();
+            promise.then(function(response) {
+                var tree = new Treant(response);
+            });
+        };
+
+        $scope.loadAndTrigger();
     }
 })();
