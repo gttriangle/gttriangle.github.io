04a1f57d0e464235b8848de5aa92ff84ef52df7b
Triton Wolfe <triton.wolfe@gmail.com>
2018-08-02 14:44:44 -0500

    add account page

--- sources/controllers/AccountCtrl.js
+++ sources/controllers/AccountCtrl.js
@@ -1,7 +1,57 @@
 (function () {
     'use strict';
-    angular.module('gt-tri').controller('AccountCtrl', ['$scope', '$state', '$stateParams', 'authSvc', AccountCtrl]);
-    function AccountCtrl($scope, $state, $stateParams, authSvc) {
+    angular.module('gt-tri').controller('AccountCtrl', ['$scope', '$state', '$stateParams', 'authSvc', 'firebase', '$firebaseArray', AccountCtrl]);
+    function AccountCtrl($scope, $state, $stateParams, authSvc, firebase, $firebaseArray) {
+        var vm = $scope.vm || {};
+        $scope.vm = vm;
+
+        var genGuid = function () {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        };
+
+        var updatePage = function () {
+            vm.transactions = [];
+            vm.total = 0;
+            var user = authSvc.loggedInUser().name;
+            vm.dbList.forEach(function (item) {
+                if (item.fromAccounts && user == item.from) {
+                    vm.transactions.push({
+                        from: item.from,
+                        boldFrom: true,
+                        to: item.to,
+                        boldTo: false,
+                        amount: item.amount,
+                        date: new Date(item.date),
+                        memo: item.memo
+                    });
+                    vm.total += parseInt(item.amount);
+                } else if (item.toAccounts && user == item.to) {
+                    vm.transactions.push({
+                        from: item.from,
+                        boldFrom: false,
+                        to: item.to,
+                        boldTo: true,
+                        amount: item.amount,
+                        date: new Date(item.date),
+                        memo: item.memo
+                    });
+                    vm.total -= parseInt(item.amount);
+                }
+            });
+        }
+
+        var promise = $firebaseArray(firebase.database().ref().child('Transactions')).$loaded();
+        promise.then(function (value) {
+            vm.dbList = value;
+            updatePage();
+            vm.dbList.$watch(function () {
+                updatePage();
+            });
+        });
+
 
     }
 })();
