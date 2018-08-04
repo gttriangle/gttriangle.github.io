ff578ff3de70dad65fecfe688cb77479f774a861
Triton Wolfe <triton.wolfe@gmail.com>
2018-08-04 06:50:50 -0500

    Create a home Page

--- sources/controllers/HomeCtrl.js
+++ sources/controllers/HomeCtrl.js
@@ -3,6 +3,6 @@
     angular.module('gt-tri').controller('HomeCtrl', ['$scope', '$state', HomeCtrl]);
 
     function HomeCtrl($scope, $state) {
-        $scope.message = "I am the home Controller";
+        
     };
 })();
